ffd18fcd35d7188f1077c26d23766bab77e3e20c
Nisha Abraham <nisha.abraham94@gmail.com>
2020-02-20 23:00:03 +0530

    Basic outline of app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main () => runApp(MyApp());
+import './transaction.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
@@ -13,14 +15,63 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transation = [
+    Transaction(
+      id: 't1',
+      title: 'New Dress',
+      amount: 1500,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Electricity Bill',
+      amount: 560,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't3',
+      title: 'Sofa Furniture',
+      amount: 45000,
+      date: DateTime.now(),
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Expense App'),
         ),
-        body: Center(
-          child: Text('Widget playground'),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.amberAccent,
+                child: Text('CHART'),
+                elevation: 5,
+              ),
+            ),
+            Column(
+              children: transation.map((tx) {
+                return Row(
+                  children: <Widget>[
+                    Container(
+                      child: Text(tx.amount.toString()),
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(tx.title),
+                        Text(tx.date.toString()),
+                      ],
+                    )
+                  ],
+                );
+              }).toList(),
+            )
+          ],
         ));
   }
 }
